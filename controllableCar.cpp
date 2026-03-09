017439fba4bb8e7169f779c1fd7fe84170a9421f
Alen Alex <alenalexarapura@gmail.com>
2026-03-09 21:41:56 +0530

    utils func getIntersection added

--- controllableCar.cpp
+++ controllableCar.cpp
@@ -1,8 +1,49 @@
 #include "raylib.h"
 #include <vector>
 #include <math.h>
+#include <iostream>
 
 using namespace std;
+
+Vector2 getIntersection(Wall wall, Vector2 sensorStart, Vector2 sensorEnd)
+{
+  Vector2 d1 = {(wall.b.x - wall.a.x), (wall.b.y - wall.a.y)};
+  Vector2 d2 = {(sensorEnd.x - sensorStart.x), (sensorEnd.y - sensorStart.y)};
+
+  float t, s;
+  t = ((sensorStart.x - wall.a.x) * d2.y - (sensorStart.y - wall.a.y) * d2.x) / ((d1.x * d2.y) - (d1.y * d2.x));
+  s = ((sensorStart.x - wall.a.x) * d1.y - (sensorStart.y - wall.a.y) * d1.x) / ((d1.x * d2.y) - (d1.y * d2.x));
+
+  Vector2 intersection;
+
+  if (t < 1 && t > 0 && s < 1 && s > 0)
+  {
+    intersection.x = wall.a.x + t * d1.x;
+    intersection.y = wall.a.y + t * d1.y;
+  }
+
+  return {intersection};
+}
+
+bool buttonClicked(Rectangle rec)
+{
+  Vector2 mouse = GetMousePosition();
+  if (CheckCollisionPointRec(mouse, rec) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+  {
+    return true;
+  }
+  return false;
+}
+
+bool buttonHover(Rectangle rec)
+{
+  Vector2 mouse = GetMousePosition();
+  if (CheckCollisionPointRec(mouse, rec))
+  {
+    return true;
+  }
+  return false;
+}
 
 class Car
 {
@@ -108,13 +149,15 @@ int main()
 
   vector<Vector2> path;
 
+  Rectangle setTrackBtn = {1750, 1000, 120, 40};
+
   while (!WindowShouldClose())
   {
     float dt = GetFrameTime();
     car.Update(dt);
 
     // path mouse input
-    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && IsKeyDown(KEY_LEFT_SHIFT))
     {
       Vector2 mouse = GetMousePosition();
       path.push_back(mouse);
@@ -126,7 +169,6 @@ int main()
     }
 
     BeginDrawing();
-
     ClearBackground(BLACK);
 
     // path drawing
@@ -136,11 +178,27 @@ int main()
       DrawCircle(path[i].x, path[i].y, 5, WHITE);
     }
 
+    // draw car
     car.Draw();
+
+    // HUD
     DrawText(TextFormat("X: %.2f", car.x), 20, 20, 20, GREEN);
     DrawText(TextFormat("Y: %.2f", car.y), 20, 45, 20, GREEN);
     DrawText(TextFormat("Speed: %.2f", car.speed), 20, 70, 20, GREEN);
     DrawText(TextFormat("Angle: %.2f", car.angle), 20, 95, 20, GREEN);
+
+    DrawRectangleRec(setTrackBtn, GRAY);
+    DrawText("Set Track", 1755, 1005, 20, BLACK);
+
+    if (buttonHover(setTrackBtn))
+    {
+      DrawRectangleRec(setTrackBtn, DARKGRAY);
+      DrawText("Set Track", 1755, 1005, 20, BLACK);
+    }
+    if (buttonClicked(setTrackBtn))
+    {
+      cout << "clicked" << endl;
+    }
     EndDrawing();
   }
   CloseWindow();
